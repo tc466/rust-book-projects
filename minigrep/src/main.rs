d97f952f5261300cff32d17dd544cdca07e10a5a
Tao Chen <tc466@cornell.edu>
2019-08-11 14:52:09 -0700

    Initial import.

---
+++ minigrep/src/main.rs
@@ -0,0 +1,20 @@
+// A simple program to search text in a file.
+
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {} in file {}", config.query, config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
